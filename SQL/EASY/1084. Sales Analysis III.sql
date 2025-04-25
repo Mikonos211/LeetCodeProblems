a6052c8f6c9eb5a144c064dfa3d08d19d1472f4b
Mikołaj Kowalski <153086381+Mikonos211@users.noreply.github.com>
2025-04-25 10:42:08 +0200

    Create 1084. Sales Analysis III.sql

---
+++ SQL/EASY/1084. Sales Analysis III.sql
@@ -0,0 +1,5 @@
+select Product.product_id , product_name   
+from Product 
+join Sales on Sales.product_id = Product.product_id 
+group by Product.product_id, product_name
+having min(sale_date) >= '2019-01-01' and max(sale_date) <= '2019-03-31';
